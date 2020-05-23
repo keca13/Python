87a8e1397cfad5018a1e8f86042787aa9389059a
jw <keca13@wp.pl>
2020-05-23 09:19:48 +0200

    Update loops_for.py

--- loops/loops_for.py
+++ loops/loops_for.py
@@ -15,3 +15,49 @@ for i in range(1,5):
   for letter in ["a","b","c"]:
     print(letter)
     
+
+#prosty przykład
+s = "Some Text"
+cnt = 0
+
+for c in s:
+    if c == "e":
+        cnt = cnt + 1
+print("found", cnt, "'e'")
+
+#przykłady na plikach
+import json
+import pickle
+import pprint
+
+
+#przykłąd z zastosowaniem danych słownika zapisanych pliku txt w formacie json
+with open("dane.txt", "r") as plik:
+        dane_plik = json.load(plik)
+        pprint.pprint(dane_plik)
+        cnt = 0
+        g = input("szukana wartosc: ")
+
+        for c in dane_plik:
+            if c == g:
+                #liczy ie wartosci
+                cnt = cnt + 1
+        print("found", cnt, g)
+        g =dane_plik[g]
+        print(g)
+
+#przykład z zastosowaniem danych słownika zapisanych pliku w formacje binarnym
+with open("dane", "rb") as plik:
+        dane_plik = pickle.load(plik)
+        pprint.pprint(dane_plik)
+        cnt = 0
+        g = input("szukana wartosc: ")
+
+        for c in dane_plik:
+            if c == g:
+                #liczy ie wartosci
+                cnt = cnt + 1
+        print("found", cnt, g)
+        g =dane_plik[g]
+        print(g)
+
