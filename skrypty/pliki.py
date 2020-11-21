b69c1546898b0fa5887f59d1d1f753f3be9eff2c
Jacek Wieteska <theesccode@gmail.com>
2020-11-21 22:02:39 +0100

    pliki

---
+++ skrypty/pliki.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import time, os
+
+def readFromFile(*path_to_files):
+    if path_to_files is not None:
+        for path in path_to_files:
+            server_file = open(path, 'r')
+            server_config = server_file.read()
+        print(server_config)
+        server_file.close()
+
+def writeToFile(*path_to_files):
+    if path_to_files is not None:
+        for path in path_to_files:
+            server_file = open(path, 'w')
+            title = "DEVELOPER MODE\n"
+            server_file.write(title)
+            # server_params = ['CPU=20\n', 'RAM=300\n', "TOTALDISK=3\n", "\tD:\\, C:\\\n"]
+            server_params = [ "CPU=20\n", "RAM=300G\n", "TOTALDISK=3\n", "\t/dev/sda, /dev/sdb\n" ]
+            server_file.write(''.join(server_params))
+        server_file.close()
+
+start_time = time.localtime()
+print("Script start time: {} {} {} ".format(start_time.tm_hour, start_time.tm_min, start_time.tm_sec))
+
+stage = os.getenv("STAGE", default='dev').upper()
+
+if stage.startswith('PROD'):
+    output = f"SERVER is in {stage} mode"
+    print(output)
+    exit()
+elif os.path.exists('./server.conf'):
+    readFromFile('./server.conf')
+else:
+    writeToFile('./server.conf')
+
+stop_time = time.localtime()
+diffrence = time.mktime(stop_time) - time.mktime(start_time)
+print("Script stopped at: {}".format(time.strftime("%X", stop_time)))
+print("Total time {} seconds".format(diffrence))
